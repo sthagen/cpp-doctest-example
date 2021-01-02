cd045cb6d0922199f10d4d68cdcf521ee2d44e7e
Stefan Hagen <stefan@hagen.link>
2021-01-02 20:38:30 +0100

    Fixed include file name

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
 #include <doctest/doctest.h>
-#include <tribonacci.h>
+#include <tribonacci.hpp>
 
 TEST_CASE("tribonacci recursive") {
     // 0, 0, 1, 1, 2, 4, 7, 13, 24, 44, 81, 149, 274, 504, 927, 1705, 3136, 5768, 10609, 19513, 35890, 66012, … (sequence A000073 in the OEIS)
